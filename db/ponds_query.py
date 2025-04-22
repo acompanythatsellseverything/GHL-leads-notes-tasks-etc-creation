f66a3b591669fd1bc409bae00acba54435c2d2a5
lebwa1337 <lebwa1337@gmail.com>
2025-04-22 15:22:37 +0300

    fix env url for ponds endpoint

--- db/ponds_query.py
+++ db/ponds_query.py
@@ -1,6 +1,8 @@
+import os
+
 import requests
 
-RETOOL_URL_FOR_SQL = "https://api.retool.com/v1/workflows/d378f48d-2398-4488-9f3a-498b6fc2b576/startTrigger?workflowApiKey=retool_wk_05c76ca96e98468f9657b32fbe3f737c"
+RETOOL_URL_FOR_SQL = os.getenv("RETOOL_URL_FOR_SQL")
 
 
 def get_ponds():
